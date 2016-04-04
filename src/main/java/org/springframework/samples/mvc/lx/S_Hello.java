70aac53f0447830074236b1bcd947e67d2a5bc7a
137289 <lixiao3@syswin.com>
2016-04-04 18:51:38 +0800

    新增测试方法

--- src/main/java/org/springframework/samples/mvc/lx/S_Hello.java
+++ src/main/java/org/springframework/samples/mvc/lx/S_Hello.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(produces="charset=UTF-8")
 public class S_Hello {
 	@RequestMapping(value = "/lx")
 	@ResponseBody
@@ -12,9 +13,15 @@ public class S_Hello {
 		return "第一个后端接口成功调用";
 	}
 	
-	@RequestMapping(value = "/seond")
+	@RequestMapping(value = "/second")
 	@ResponseBody
 	public String getSecond(){
-		return "second";
+		return "第二个也成功！";
+	}
+	
+	@RequestMapping(value = "/p")
+	@ResponseBody
+	public String getParam(String name,int age){
+		return name + age;
 	}
 }
